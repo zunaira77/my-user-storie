47e8b7fd0669407fb497a29ed11c29922a370f15
MirjamBibus <bibum97@zedat.fu-berlin.de>
2020-07-12 23:10:31 +0200

    Menüleiste

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('post/<int:pk>/', views.post_detail, name='post_detail'),     
     path('post/new/', views.post_new, name='post_new'), 
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+    path('', views.post_easy, name='post_easy'),
+    path('', views.post_medium, name='post_medium'),
+    path('', views.post_hard, name='post_hard'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -8,7 +8,19 @@ from .forms import PostForm
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
-    
+
+def post_easy(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_easy.html', {'posts': posts})
+
+def post_medium(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_medium.html', {'posts': posts})
+
+def post_hard(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_hard.html', {'posts': posts})
+
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
